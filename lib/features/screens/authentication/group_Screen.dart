201e3cdefca4a61e67c3b25c7e2d529513de059c
Han Seung Min <116543259+seungminhan2631@users.noreply.github.com>
2025-02-12 20:35:25 +0900

    asd
    asd

--- lib/features/screens/authentication/group_Screen.dart
+++ lib/features/screens/authentication/group_Screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:msdl/commons/widgets/toptitle.dart';
+import 'package:msdl/constants/gaps.dart';
 
-class GroupScreen extends StatelessWidget {
+class GroupScreen extends StatefulWidget {
   const GroupScreen({super.key});
 
   @override
+  State<GroupScreen> createState() => _GroupScreenState();
+}
+
+class _GroupScreenState extends State<GroupScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    String todayDate = DateFormat('yyyy.MM.dd', 'ko_KR')
+        .format(DateTime.now()); // ✅ 대한민국 날짜 형식 적용
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 70.0, horizontal: 60.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TopTitle(text: "Group"), // ✅ 왼쪽 정렬
+            Text(
+              todayDate, // ✅ 현재 날짜 표시
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:msdl/features/screens/Home/homeScreen.dart';
 import 'package:msdl/features/screens/authentication/choose_role_Screen.dart';
+import 'package:msdl/features/screens/authentication/group_Screen.dart';
 import 'package:msdl/features/screens/authentication/signUp_Screen.dart';
 import 'package:msdl/features/screens/authentication/login_Screen.dart';
 import 'package:msdl/features/screens/settings/setting_Screen.dart';
 import 'package:msdl/msdl_theme.dart';
+import 'package:intl/date_symbol_data_local.dart'; // ✅ 날짜 포맷 데이터 초기화 패키지
 
-void main() {
-  runApp(const msdl());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // ✅ Flutter 엔진 초기화 (비동기 코드 사용 가능)
+  await initializeDateFormatting('ko_KR', null); // ✅ 대한민국 날짜 포맷 초기화
+  runApp(msdl());
 }
 
 class msdl extends StatelessWidget {
@@ -27,7 +31,7 @@ class msdl extends StatelessWidget {
           ),
           initialRoute: "/",
           routes: {
-            "/": (context) => SettingsScreen(),
+            "/": (context) => GroupScreen(),
             "/chooseRole_Screen": (context) => ChooseRoleScreen(),
             "/createAccount_Screen": (context) => SignupScreen(),
             "/SignupScreen": (context) => SignupScreen(),
