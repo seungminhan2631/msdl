0caa20ec670e69cb0cdf23e7096cf0bbd28bf9bb
Han Seung Min <116543259+seungminhan2631@users.noreply.github.com>
2025-02-20 10:17:07 +0900

    update
    update

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ class msdl extends StatelessWidget {
           ),
           initialRoute: "/",
           routes: {
-            "/": (context) => LoginScreen(),
+            "/": (context) => WorkplaceScreen(),
             "/chooseRole_Screen": (context) => ChooseRoleScreen(),
             "/nameScreen": (context) => NameScreen(),
             "/createAccount_Screen": (context) => SignupScreen(),
