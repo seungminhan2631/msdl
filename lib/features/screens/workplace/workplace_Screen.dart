802b43e5887cf76534c0a24155e6ba7315f5e2c3
박찬우 <116543427+parkchanwu@users.noreply.github.com>
2025-02-23 03:29:26 +0900

    update
    update

--- lib/features/screens/workplace/workplace_Screen.dart
+++ lib/features/screens/workplace/workplace_Screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sort_child_properties_last
+
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -9,9 +11,6 @@ import 'package:msdl/commons/widgets/buttons/customButton.dart';
 import 'package:msdl/constants/gaps.dart';
 import 'package:msdl/constants/size_config.dart';
 import 'package:msdl/constants/sizes.dart';
-import 'package:msdl/features/screens/workplace/widget/DraggableBottomSheet.dart'
-    as bottom;
-import 'package:msdl/features/screens/workplace/widget/boxInBottomBar.dart';
 
 class WorkplaceScreen extends StatefulWidget {
   @override
@@ -26,12 +25,19 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
   String _currentAddress = "Loading location...";
   double _markerYOffset = 0;
   String? _darkMapStyle;
+  double _floatingButtonOffset = 100;
 
   @override
   void initState() {
     super.initState();
     _determinePosition();
     _loadMapStyle();
+
+    _sheetController.addListener(() {
+      setState(() {
+        _floatingButtonOffset = 200 + (_sheetController.size - 0.28) * 600;
+      });
+    });
   }
 
   Future<void> _loadMapStyle() async {
@@ -39,25 +45,55 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
   }
 
   Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // 위치 서비스 활성화 여부 확인
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // 위치 서비스가 꺼져 있는 경우
+      return Future.error('Location services are disabled.');
+    }
+
+    // 위치 권한 확인
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // 사용자가 권한을 거부한 경우
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // 사용자가 "다시 묻지 않음"을 선택한 경우
+      return Future.error('Location permissions are permanently denied.');
+    }
+
+    // 현재 위치 가져오기
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
-    maps.LatLng newPosition =
-        maps.LatLng(position.latitude, position.longitude);
+
     setState(() {
-      _currentPosition = newPosition;
+      _currentPosition = maps.LatLng(position.latitude, position.longitude);
     });
-    _updateAddress(newPosition);
-    _mapController.animateCamera(maps.CameraUpdate.newLatLng(newPosition));
   }
 
   Future<void> _updateAddress(maps.LatLng position) async {
     try {
       List<Placemark> placemarks =
           await placemarkFromCoordinates(position.latitude, position.longitude);
+
       if (placemarks.isNotEmpty) {
+        String buildingName = placemarks[0].name ?? "";
+        String streetName = placemarks[0].street ?? "";
+        String locality = placemarks[0].locality ?? "";
+        String country = placemarks[0].country ?? "";
+
         setState(() {
-          _currentAddress =
-              "${placemarks[0].street}, ${placemarks[0].locality}, ${placemarks[0].country}";
+          _currentAddress = buildingName.isNotEmpty ? buildingName : streetName;
+          if (locality.isNotEmpty) _currentAddress += ", $locality";
+          if (country.isNotEmpty) _currentAddress += ", $country";
         });
       }
     } catch (e) {
@@ -65,6 +101,20 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
         _currentAddress = "주소를 찾을 수 없음";
       });
     }
+  }
+
+  Future<void> _goToCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    maps.LatLng newPosition =
+        maps.LatLng(position.latitude, position.longitude);
+
+    setState(() {
+      _currentPosition = newPosition;
+    });
+
+    _mapController.animateCamera(maps.CameraUpdate.newLatLng(newPosition));
+    _updateAddress(newPosition);
   }
 
   void _onMapCreated(maps.GoogleMapController controller) {
@@ -92,6 +142,7 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
                 zoom: 14.0,
               ),
               onMapCreated: _onMapCreated,
+              zoomControlsEnabled: false,
               onCameraMove: (maps.CameraPosition position) {
                 setState(() {
                   _markerYOffset = -10;
@@ -108,8 +159,8 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
           ),
 
           Positioned(
-            top: 40,
-            left: 10,
+            top: Sizes.size40,
+            left: Sizes.size20,
             child: GestureDetector(
               onTap: () {
                 Navigator.pushNamed(context, "/homeScreen");
@@ -134,6 +185,21 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
             ),
           ),
 
+          Positioned(
+            bottom: _floatingButtonOffset,
+            right: Sizes.size5,
+            child: FloatingActionButton(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+              child: Icon(
+                Icons.my_location_rounded,
+                color: Colors.red,
+              ),
+              onPressed: _goToCurrentLocation,
+            ),
+          ),
+
           // 하단 현재 위치 정보 패널
           DraggableScrollableSheet(
             controller: _sheetController,
@@ -142,7 +208,7 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
             maxChildSize: 0.4,
             builder: (context, scrollController) {
               return Container(
-                padding: EdgeInsets.symmetric(vertical: 10),
+                padding: EdgeInsets.symmetric(vertical: Sizes.size10),
                 decoration: BoxDecoration(
                   border: Border(
                     top: BorderSide(
@@ -162,19 +228,19 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Container(
-                        width: 80,
-                        height: 3,
+                        width: Sizes.size40 + Sizes.size40,
+                        height: Sizes.size3,
                         decoration: BoxDecoration(
                           color: Color(0xFFD9D9D9),
                           borderRadius: BorderRadius.circular(10),
                         ),
                       ),
-                      SizedBox(height: 6),
+                      Gaps.v8,
                       Text(
                         "Current location : ",
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 16,
+                          fontSize: Sizes.size16,
                           fontFamily: "Andika",
                         ),
                       ),
@@ -183,10 +249,11 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 14,
+                          fontSize: Sizes.size14,
                           fontFamily: "Andika",
                         ),
                       ),
+                      Gaps.v28,
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
@@ -201,7 +268,7 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
                             iconColor: Color(0xFF3F51B5),
                           ),
                           _BoxInBottomBar(
-                            text: "Out Of Office (OOO)",
+                            text: "Off-Site",
                             icon: Icons.business_center_outlined,
                             iconColor: Color(0xFF935E38),
                           ),
@@ -212,7 +279,7 @@ class _WorkplaceScreenState extends State<WorkplaceScreen> {
                           ),
                         ],
                       ),
-                      SizedBox(height: 10.h),
+                      Gaps.v32,
                       CustomButton(
                         text: "Add New Workplace",
                         routeName: "/homeScreen",
@@ -243,22 +310,28 @@ class _BoxInBottomBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 85.w,
-      height: 100.h,
+      width: Sizes.size80 + Sizes.size5,
+      height: Sizes.size80 + Sizes.size20,
       decoration: BoxDecoration(
           color: const Color(0xFF2C2C2C),
           borderRadius: BorderRadius.circular(5),
-          border: Border.all(color: const Color(0xFFAAAAAA), width: 1.w)),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Icon(icon, color: iconColor),
-        const SizedBox(height: 5),
-        Text(text,
-            style: const TextStyle(
+          border:
+              Border.all(color: const Color(0xFFAAAAAA), width: Sizes.size1)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: iconColor),
+          Gaps.v5,
+          Text(
+            text,
+            style: TextStyle(
                 color: Colors.white,
                 fontFamily: "Andika",
                 fontWeight: FontWeight.w700,
-                fontSize: 15))
-      ]),
+                fontSize: Sizes.size14 + Sizes.size1),
+          )
+        ],
+      ),
     );
   }
 }
